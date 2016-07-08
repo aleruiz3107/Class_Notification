dca8ac11c302aef2b204c6e36ff3bded625d0bda
AlejandroRuiz <alejandroruiz@alejandroruizs-mac.local>
2016-07-08 18:20:53 -0500

    Initial Commit

---
+++ Class_Notification/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Class_Notification
+//
+//  Created by AlejandroRuiz on 7/8/16.
+//  Copyright © 2016 AlejandroRuiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
